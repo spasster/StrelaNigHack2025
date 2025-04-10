fcd304dd9e96c8d5ddffbce5f10fb5c7348e05a7
Danila <72632009+spasster@users.noreply.github.com>
2025-04-10 12:18:36 +0300

    Update settings.py

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -116,7 +116,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'Strela2025',
-        'USER': 'scrait',
+        'USER': 'postgres',
         'PASSWORD': 'sex123',
         'HOST': os.getenv('DB_HOST', 'localhost'),
         'PORT': '5432'
